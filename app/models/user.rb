a9aa464fbaa85ef4f4375f328f1048e3e19588d4
takuuo2 <126545872+takuuo2@users.noreply.github.com>
2023-05-05 10:42:24 +0000

    [ADD] iine

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20,minimum: 2}
   validates :introduction, length: {maximum: 50}
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' => "homes#about", as: "about"
-  resources :books, only: [:new, :create, :edit, :index, :show, :destroy, :update]
+  resources :books, only: [:new, :create, :edit, :index, :show, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
